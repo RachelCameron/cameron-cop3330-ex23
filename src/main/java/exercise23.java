dc1971163e52d329f7087f8269f3c8a284469524
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 23:05:36 -0400

    Initial commit

---
+++ src/main/java/exercise23.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import java.util.Scanner;
+
+public class exercise23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
